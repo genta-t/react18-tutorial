3e04224edc3e394327cf9c20fedf889175b33024
genta-t <g.tanowaki@hyo-i.com>
2023-12-01 22:06:16 +0900

    useEffect完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-
-import './App.css';
+import UseEffect from './components/UseEffect';
 import UseState from './components/UseState';
+
 
 const App = () => {
   return (
     <>
       <UseState />
+      <UseEffect />
     </>
   );
 }
