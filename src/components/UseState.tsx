dbe10ec62055e4d34632a43db39afbf22fa51ffa
genta-t <g.tanowaki@hyo-i.com>
2023-12-01 21:53:01 +0900

    useState完了

---
+++ src/components/UseState.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const UseState = () => {
+  const [ count , setCount ] = useState(0);
+  
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  
+  return (
+    <>
+      <h2>useState</h2>
+      <button onClick={handleClick}>+</button>
+      <p>{count}</p>
+    </>
+  );
+}
+
+export default UseState;
