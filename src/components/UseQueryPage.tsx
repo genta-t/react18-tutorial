93422b6017a0b5c9893c9d680f2b2768590e9bb7
genta-t <g.tanowaki@hyo-i.com>
2023-12-14 12:55:43 +0900

    useQueryをuseEffectで書くと...

--- src/components/UseQueryPage.tsx
+++ src/components/UseQueryPage.tsx
@@ -1,5 +1,6 @@
 import { useQuery } from 'react-query';
 import PageLinks from './items/PageLinks';
+import { useEffect, useState } from 'react';
 
 // useQueryはAPIなどの　データ取得に使う
 const UseQueryPage = () => {
@@ -36,4 +37,55 @@ type TypesData = {
   id: string;
   title: string;
   body: string;
+}
+
+// useState, useEffectで書くとこうなる
+
+const UseEffect = () => {
+  const [ posts, setPosts ] = useState([]);
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [ isError, setIsError ] = useState(false);
+
+  const fetchTodos =  async () => {
+    try {
+      setIsLoading(true);
+      console.log("取得中");
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      if (res.ok) {
+        const results = await res.json();
+        console.log("完了");
+        setPosts(results);
+      } else {
+        setIsError(true);
+      }
+    } catch (error) {
+      setIsError(true);
+    }
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+  
+  if (isError) return <p>エラーが出ています</p>;
+
+  return(
+    <>
+      <p>useEffect</p>
+      { isLoading ? (
+        <p>ローディング中</p>
+      ) : (
+        posts.map((p: TypesData, i: number) => {
+          return (
+            <div key={i}>
+              <p>{p.id}</p>
+              <p>{p.title}</p>
+              <p>{p.body}</p>
+            </div>
+          );
+        }
+      ))}
+    </>
+  )
 }
